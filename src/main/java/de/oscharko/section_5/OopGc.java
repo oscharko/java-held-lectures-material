4d9a89f8493161e2d1a5055fd3bd270e36267110
oscharko <o.scharkowski@oscharko.de>
2022-07-06 09:59:19 +0200

    new class in section 5 OopGc

---
+++ src/main/java/de/oscharko/section_5/OopGc.java
@@ -0,0 +1,14 @@
+package de.oscharko.section_5;
+
+public class OopGc {
+    public static void main(String[] args) {
+        /*java.util.Stack stack = new java.util.Stack();*/
+/*        while (true) {
+            *//*stack.push(new java.awt.Point());*//*
+            new java.awt.Point();
+        }*/
+        java.awt.Point p;
+        p = new java.awt.Point();
+        System.out.println(p);
+    }
+}
